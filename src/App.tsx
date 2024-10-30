50354aacd0bcff93c4cdee856c69cb21d400bae0
brendamareco <brendaamareco@gmail.com>
2024-10-30 20:42:33 -0300

    added chooseSpecialsPage and chooseModifiersPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraBaseProvider, extendTheme } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ChooseClassPage } from "./pages/ChooseClassPage";
@@ -9,7 +10,8 @@ import { AudioPlayerProvider } from "./providers/AudioPlayerProvider";
 import { CardAnimationsProvider } from "./providers/CardAnimationsProvider";
 import { GameProvider } from "./providers/GameProvider";
 import customTheme from "./theme/theme";
-import { GamePage } from "./pages/Game/GamePage";
+import { ChooseModifiersPage } from "./pages/ChooseModifiersPage";
+import { ChooseSpecialsPage } from "./pages/ChooseSpecialsPage";
 
 function App() {
   const theme = extendTheme(customTheme);
@@ -21,7 +23,11 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/choose-class" element={<ChooseClassPage />} />
-              <Route path="/game" element={<GamePage />} />
+              <Route
+                path="/choose-modifiers"
+                element={<ChooseModifiersPage />}
+              />
+              <Route path="/choose-specials" element={<ChooseSpecialsPage />} />
             </Routes>
           </AudioPlayerProvider>
         </GameProvider>
