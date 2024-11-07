e7e92f4b090d7c88deab02ea7f455c3b3903bef9
brendamareco <brendaamareco@gmail.com>
2024-11-07 01:48:26 -0300

    changed getChallenge implementation

--- src/dojo/queries/useChallenge.tsx
+++ src/dojo/queries/useChallenge.tsx
@@ -3,6 +3,7 @@ import { Entity } from "@dojoengine/recs";
 import { getEntityIdFromKeys } from "@dojoengine/utils";
 import { useDojo } from "../useDojo";
 import { getLSGameId } from "../utils/getLSGameId";
+import { useEffect, useState } from "react";
 
 export const useChallengePlayer = () => {
   const {
@@ -17,20 +18,43 @@ export const useChallengePlayer = () => {
 
 export const useChallenge = () => {
   const {
-    setup: {
-      clientComponents: { Challenge },
-    },
+    setup: { client },
   } = useDojo();
-  const gameId = getLSGameId();
-  const entityId = getEntityIdFromKeys([BigInt(gameId)]) as Entity;
-  const challengeResult = useComponentValue(Challenge, entityId);
-  const challenges = (challengeResult?.active_ids ?? []).map((challenge) => {
-    const id = challenge[0] ? Number((challenge[0] as any)?.value) : 0;
+
+  const [challenges, setChallenges] = useState<
+    { id: number; completed: boolean }[]
+  >([]);
+
+  useEffect(() => {
+    fetchChallenges();
+  }, []);
+
+  const fetchChallenges = () => {
+    getChallenges(client).then((newChallenges) => {
+      // console.log("challenge: ", newChallenges);
+
+      setChallenges(newChallenges);
+    });
+  };
+
+  return challenges;
+};
+
+export const getChallenges = async (
+  client: any
+): Promise<{ id: number; completed: boolean }[]> => {
+  const result = await client.player_system.get_challenge({
+    game_id: getLSGameId(),
+  });
+
+  return (result.active_ids as any).map((challenge: any) => {
+    const id = challenge[0] ? Number((challenge[0] as number)?.valueOf()) : 0;
+
+    console.log(challenge[1]);
+
     const completed = challenge[1]
-      ? Boolean((challenge[1] as any)?.value)
+      ? Boolean((challenge[1] as boolean)?.valueOf())
       : false;
     return { id, completed };
   });
-
-  return challenges;
 };
